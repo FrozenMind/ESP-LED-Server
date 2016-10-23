b5f52aced0349c4e55e838edec86b600dae59913
Christian Bechtluft <development@fam-bechtluft.de>
2016-10-23 21:17:00 +0200

    Client Verbesserung v2
    Anpassung an Server
    ESP Objekt entält jetzt debugId anstatt debugUser

--- public/client/esp.js
+++ public/client/esp.js
@@ -1,7 +1,7 @@
-function ESP(id, color, mode, debug, debugUser){
+function ESP(id, color, mode, debug, debugId){
     this.Id = id;
     this.Color = color;
     this.Mode = mode;
     this.DebugEnabled = (typeof debug === 'undefined') ? false : debug;
-    this.DebugUser = debugUser;
+    this.DebugId = debugId;
 }
