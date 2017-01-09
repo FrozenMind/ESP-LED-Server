729aa711a9f8db3a94e331840e96644157e56c5f
FrozenMind <valentin.rebscher@live.de>
2017-01-09 21:40:58 +0100

    fixed pingpongdouble

--- lua/PingPongDouble.lua
+++ lua/PingPongDouble.lua
@@ -8,57 +8,30 @@ Indigo = string.char(0,75,130)
 Violet = string.char(0,127,255)
 
 colors = {Red,Orange,Yellow,Green,Blue,Indigo,Violet}
-arraysize = 7
-
-index = 1
 
 ws2812.init()
 numberofleds = 16
-startPos = math.floor(numberofleds / 2)
-Color = colors[1]
-print(Color)
 buffer = ws2812.newBuffer(numberofleds, 3)
 
 shiftForward = true
-
-playerOnePos = startPos+1
-playerTwoPos = startPos
-
-playerOneDirection = 1
-playerTwoDirection = -1
-
-tmr.alarm(0,50,tmr.ALARM_AUTO, function()
-
-    if(playerOnePos == numberofleds) then
-        playerOneDirection = -1 * playerOneDirection
+pos = 0
+tmr.alarm(0,100,tmr.ALARM_AUTO, function()
+    if(pos == numberofleds - 1) then
+        shiftForward = false                
+    end
+    if(pos == 0) then
+        shiftForward = true        
     end
 
-    if(playerOnePos == playerTwoPos) then
-        playerOneDirection = -1 * playerOneDirection
-        playerTwoDirection = -1 * playerTwoDirection
-        CalcIndex()
-        SwitchColor(index)
+    if(shiftForward) then
+        pos = pos + 1
+    else
+        pos = pos - 1
     end
-    
-    if(playerTwoPos==1) then
-        playerTwoDirection = -1 * playerTwoDirection
-    end
-
-    playerOnePos = playerOnePos + playerOneDirection
-    playerTwoPos = playerTwoPos + playerTwoDirection
-
-    print(Color)
-    
     ws2812.write(buffer)
     buffer:fade(2)
-    buffer:set(playerOnePos, Color)
-    buffer:set(playerTwoPos, Color)
+    buffer:set(pos + 1, Color)
+    buffer:set(numberofleds - pos, Color)
 end)
 
-function CalcIndex()
-    index = ((index+1) % arraysize) + 1
-end
 
-function SwitchColor(index)
-    Color = colors[index]
-end
